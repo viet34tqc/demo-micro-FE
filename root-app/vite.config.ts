20fe984b0b1dffd8042680d51699d9db4ff958fb
viet34tqc <viet34tqc@gmail.com>
2024-01-12 16:51:24 +0700

    Apply env variable

--- root-app/vite.config.ts
+++ root-app/vite.config.ts
@@ -1,41 +1,40 @@
 import federation from '@originjs/vite-plugin-federation';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: 'root-app',
-      // This file name is optional, default is remoteEntry.js
-      // I set it like this to let other modules get the name of this module more clearly
-      filename: 'root-app-entry.js',
-      // Shared modules will be loaded only once to avoid duplications.
-      // Zustand is also used in product app
-      shared: ['react', 'react-dom', 'react-router-dom', 'zustand'],
-      // Use product from product module
-      remotes: {
-        'product-app':
-          'http://localhost:3001/product-app/dist/assets/product-app-entry.js',
-        'cart-app':
-          'http://localhost:3002/cart-app/dist/assets/cart-app-entry.js',
-        'store-app':
-          'http://localhost:3003/store-app/dist/assets/store-app-entry.js',
-        'about-app':
-          'http://localhost:4001/about-app/dist/assets/about-app-entry.js',
-      },
-    }),
-  ],
-  build: {
-    modulePreload: false,
-    target: 'esnext',
-    minify: false,
-    cssCodeSplit: false,
-  },
-  server: {
-    port: 3000,
-  },
-  preview: {
-    port: 3000,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: 'root-app',
+        // This file name is optional, default is remoteEntry.js
+        // I set it like this to let other modules get the name of this module more clearly
+        filename: 'root-app-entry.js',
+        // Shared modules will be loaded only once to avoid duplications.
+        // Zustand is also used in product app
+        shared: ['react', 'react-dom', 'react-router-dom', 'zustand'],
+        // Using env variable when we deploy to server
+        remotes: {
+          'product-app': env.VITE_PRODUCT_APP,
+          'cart-app': env.VITE_CART_APP,
+          'store-app': env.VITE_STORE_APP,
+          'about-app': env.VITE_ABOUT_APP,
+        },
+      }),
+    ],
+    build: {
+      modulePreload: false,
+      target: 'esnext',
+      minify: false,
+      cssCodeSplit: false,
+    },
+    server: {
+      port: 3000,
+    },
+    preview: {
+      port: 3000,
+    },
+  };
 });
